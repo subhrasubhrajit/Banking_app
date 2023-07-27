2cc3691336e482791155d29093c409c24361734c
MaDeproGraMer <nandysubhrajit12@gmail.com>
2023-07-27 20:37:48 +0530

    some update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { navLinks } from "../constants/data";
 const Navbar = () => {
     const [active, setActive] = useState("Home");
     const [toggle, setToggle] = useState(false);
+
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
             <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
